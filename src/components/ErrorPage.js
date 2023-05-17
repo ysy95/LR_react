f619088b5d8edb7883862df2021f86578e4ac0af
Nam-b <121988406+Nam-b@users.noreply.github.com>
2023-05-17 17:51:05 +0900

    0517

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,10 +1,30 @@
 import React from 'react';
-
+import './css/error.css';
+// import {Link} from 'react-router-dom';
 function ErrorPage(props) {
   return (
-    <div>
-      404
-    </div>
+    <>
+    <section className='err_wrap'>
+      <article className='tit_wrap'>
+        <h2>404 ERROR</h2>
+        <p>Page No Found</p>
+        <div className='txt_wrap'>
+          <p>페이지가 없거나 오류가 발생하였습니다</p>
+          <p>현재 페이지가 존재하지 않거나
+            ,<p>현재 이용할 수 없는 페이지 입니다.</p>
+          </p>
+          </div>
+          <a href='/' title='메인페이지가기'>
+            홈으로 가기
+          </a>
+      </article>
+      <article className='imgs_wrap'>
+      <img src={process.env.PUBLIC_URL + `/images/u_common/error.gif`}
+      alt={'에러메세지'}
+      />
+      </article>
+    </section>
+    </>
   );
 }
 
